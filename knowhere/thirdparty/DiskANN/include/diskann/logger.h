74d7f33d5eba1d9d326ceafc99716e26c8257a76
Jiakang Li <1982973961@qq.com>
2026-09-27 00:10:09 +0800

    update

--- knowhere/thirdparty/DiskANN/include/diskann/logger.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-#include <iostream>
-
-namespace diskann {
-  extern std::basic_ostream<char> cout;
-  extern std::basic_ostream<char> cerr;
-}  // namespace diskann
